623f8289ba410f6372964a42e932d42225607e86
unknown <optimistka30@mail.ru>
2021-05-03 00:19:09 +0300

    Added react in project.Realized sendin messages from textarea to the array using button

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import './index.css'
+
+
+
+const App = () => {
+   const [messages, setMessages] = useState([]);
+   const myRef = React.createRef();
+
+
+   let addComment = () => {
+      let messageValue = myRef.current.value;
+      let comment = [...messages, messageValue];
+      setMessages(comment);
+      myRef.current.value = '';
+   }
+
+   return (
+      <div className="boxMessage">
+         <textarea ref={myRef}></textarea>
+         <button onClick={addComment}>Send</button>
+         <div>
+            {messages.map((item, index) => <p key={index.toString()}>{item}</p>)}
+         </div>
+
+      </div>
+
+   );
+};
+
+ReactDOM.render(<App />, document.getElementById("app"));
